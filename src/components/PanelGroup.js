92befb08264f270e2b3e5a37c5666e5ce5795bb5
云 <oncwnuLGu9BAby_Iy8qKYwvxDV00@git.weixin.qq.com>
2025-09-07 10:16:35 +0800

    fix: update panel-resize-js npm

--- src/components/PanelGroup.js
+++ src/components/PanelGroup.js
@@ -1,22 +1,29 @@
 import { useEffect, useRef } from 'react';
 import PanelResize from 'panel-resize-js';
+import Panel from './Panel';
 
-function PanelGroup (props)  {
+function PanelGroup(props) {
   const parentEle = useRef(null);
-  const { direction, children } = props;
+  const { direction, children, customCursor, panelRef = { current: undefined }, autoSaveId, className } = props;
 
   useEffect(() => {
-    const sizes = Array.from(children).map((child) => {
+    const panelEles = Array.from(children).filter(child => {
+      return child?.type === Panel;
+    });
+    const panelSizeData = Array.from(panelEles).map((child) => {
       const { defaultSize, minSize, maxSize } = child.props;
       return {
         minSize, defaultSize, maxSize
       }
     });
-    const panelSizeData = sizes.filter((it) => it.minSize || it.defaultSize);
-    new PanelResize(parentEle.current, panelSizeData, direction)
+
+    panelRef.current = new PanelResize(parentEle.current, { sizeData: panelSizeData, direction, customCursor, autoSaveId });
+    panelRef.current.setLayout([{defaultSize: 30}, {defaultSize: 30}, {defaultSize: 40 }])
   }, []);
 
 
-  return <div ref={parentEle} className='wrapper'>{children}</div>
+
+
+  return <div ref={parentEle} className={`wrapper ${className || ''}`}>{children}</div>
 }
 export default PanelGroup;
